37b1b8e651a7eab4cb1f769c8b07f7687b97511d
dviramontes <dviramontes@gmail.com>
2016-08-21 13:40:24 -0600

    adds second pluck in thread

--- dpluck.rb
+++ dpluck.rb
@@ -1,14 +1,34 @@
 live_loop :f do
-  with_synth :pluck do
-    with_fx :distortion do |r|
-      s = play_chord chord(:C5, :m7),
-        release: (ring 0.3, 0.12, 0.5).tick, amp: 1, reps:2 , attack: [2,0.5,1].choose
-      sleep 0.25
-      control s, mix: 0.93, reps: [33, 0 ,10, 44].choose, amp: 4, cutoff: (ring 0.3, 0.12, 0.5).tick
-      sleep 0.15
+  
+  in_thread do
+    with_synth :pluck do
+      with_fx :distortion do |r|
+        s = play_chord chord(:C5, :m7),
+          release: 5
+        sleep 5
+      end
     end
   end
+  
+  ##| comment do
+  in_thread do
+    with_synth :pluck do
+      with_fx :distortion do |r|
+        ss = play_chord chord(:C5, :m7),
+          release: (ring 0.3, 0.12, 0.5).tick, amp: 1, reps:1 , attack: [0.7,0.5,1].choose, release: [2, 0, 1].choose
+        sleep 0.5
+        control ss, mix: 0.93, reps: [33, 66 ,11, 44].choose, amp: 10, cutoff: (ring 0.3, 0.12, 0.5).tick
+      end
+    end
+  end
+  ##| end
+  
+  sleep 1
 end
+
+
+
+
 
 
 
