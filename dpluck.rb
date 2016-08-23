06ec28d86f50a4b0fd43e0fd5261ff8cba7afbfe
dviramontes <dviramontes@gmail.com>
2016-08-23 17:51:53 -0600

    sometimes play

--- dpluck.rb
+++ dpluck.rb
@@ -1,7 +1,7 @@
 live_loop :f do
   
   in_thread do
-    with_synth :pluck do
+    with_synth  :pluck do
       with_fx :distortion do |r|
         s = play_chord chord(:C5, :m7),
           release: 5
@@ -14,17 +14,38 @@ live_loop :f do
   in_thread do
     with_synth :pluck do
       with_fx :distortion do |r|
+        
         ss = play_chord chord(:C5, :m7),
-          release: (ring 0.3, 0.12, 0.5).tick, amp: 1, reps:1 , attack: [0.7,0.5,1].choose, release: [2, 0, 1].choose
-        sleep 0.5
-        control ss, mix: 0.93, reps: [33, 66 ,11, 44].choose, amp: 10, cutoff: (ring 0.3, 0.12, 0.5).tick
+          release: (ring 0.3, 0.12, 0.5, 12).tick,
+          divisor: rrand(50, 100),
+          cutoff: (ring 0.1, 50).tick,
+          amp: [5, 2, 1, 3, 0, 7, 4, 8].choose,
+          attack: [0.7,0.5,1].choose
+        
+        sleep 0.25
+        
+        control ss,
+          mix: 0.33,
+          reps: [33, 6 ,11, 44].choose,
+          amp: [5, 2, 1, 3, 0].choose,
+          cutoff: (ring 3, 50, 100).tick
+        
+        sleep 0.115
+        
+        control ss,
+          mix: 0.99,
+          reps: 20,
+          amp: [2, 0, 1].choose,
+          cutoff: 0.5
+        
       end
     end
   end
   ##| end
-  
   sleep 1
 end
+
+
 
 
 
