43e012bc6be94fe49c9e2c28e19c9c02811247ec
dviramontes <dviramontes@gmail.com>
2016-08-30 21:14:14 -0600

    fixes ring.tick

--- dpluck.rb
+++ dpluck.rb
@@ -1,49 +1,66 @@
+
+releases = (ring 0.3, 0.12, 0.5, 12)
+cutoffs  = (ring 0.0)
+times = (ring 1, 0.75, 5, 0.25, 0.125, 5)
+
 live_loop :f do
+  
+  use_bpm 124
   
   in_thread do
     with_synth  :pluck do
       with_fx :distortion do |r|
         s = play_chord chord(:C5, :m7),
           release: 5
+        
         sleep 5
+        
+        sample :loop_tabla,
+          start: 0.6,
+          finish: [0.7, 1, 0.66, 0.7, 1, 0.66].choose,
+          rate: 25
+        
       end
     end
   end
   
-  ##| comment do
   in_thread do
+    
     with_synth :pluck do
       with_fx :distortion do |r|
         
         ss = play_chord chord(:C5, :m7),
-          release: (ring 0.3, 0.12, 0.5, 12).tick,
-          divisor: rrand(50, 100),
-          cutoff: (ring 0.1, 50).tick,
-          amp: [5, 2, 1, 3, 0, 7, 4, 8].choose,
+          release: releases.tick,
+          cutoff: cutoffs.tick,
+          amp: [5, 2, 1, 3, 0, 5, 4, 2].choose,
           attack: [0.7,0.5,1].choose
         
-        sleep 0.25
+        sleep 2
         
         control ss,
           mix: 0.33,
           reps: [33, 6 ,11, 44].choose,
           amp: [5, 2, 1, 3, 0].choose,
-          cutoff: (ring 3, 50, 100).tick
+          cutoff: cutoffs.tick
         
-        sleep 0.115
+        sleep 1
         
         control ss,
           mix: 0.99,
           reps: 20,
-          amp: [2, 0, 1].choose,
-          cutoff: 0.5
+          amp: [10, 0].choose,
+          cutoff: 0.25
         
       end
     end
   end
-  ##| end
+  
   sleep 1
+  
 end
+
+
+
 
 
 
